bd9a46ea84325b5a9a0b2a38fdf0035b3295305b
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2024-09-17 23:54:19 +0200

    Создала функцию создания, отрисовки и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,37 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(cardData, onDelete) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const imageElement = cardElement.querySelector('.card__image');
+  const titleElement = cardElement.querySelector('.card__title');
+  const deleteButtonElement = cardElement.querySelector('.card__delete-button');
+
+  imageElement.src = cardData.link;
+  imageElement.alt = cardData.name;
+  titleElement.textContent = cardData.name;
+
+  deleteButtonElement.addEventListener('click', () => {
+    onDelete(cardElement);
+  });
+
+  return cardElement;
+};
+
+function renderCards(cardsData, onDelete) {
+  const cardsList = document.querySelector('.places__list');
+
+  cardsData.forEach(cardData => {
+    const card = createCard(cardData, onDelete);
+    cardsList.append(card);
+  });
+}
+
+function handleDelete(cardElement) {
+  cardElement.remove();
+}
+
+renderCards(initialCards, handleDelete);
