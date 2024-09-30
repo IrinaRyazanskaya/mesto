1adb2fd71309fabbe3820fa9f28730437afd2dc6
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2024-09-30 02:43:07 +0200

    Сделала рефакторинг кода

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './pages/index.css';
 import { initialCards } from './scripts/cards';
-import { openPopup, closePopup, fillEditForm, handleEditFormSubmit, handleNewCardSubmit } from './scripts/modal';
+import { openPopup, closePopup } from './scripts/modal';
 import { createCard, deleteCard, toggleLike } from './scripts/card';
 
 const cardsContainer = document.querySelector('.places__list');
@@ -14,13 +14,51 @@ const editPopup = document.querySelector('.popup_type_edit');
 const newCardPopup = document.querySelector('.popup_type_new-card');
 
 const editFormElement = editPopup.querySelector('.popup__form');
+const nameInput = editFormElement.querySelector('.popup__input_type_name');
+const jobInput = editFormElement.querySelector('.popup__input_type_description');
+
 const newCardFormElement = newCardPopup.querySelector('.popup__form');
+const titleInput = newCardFormElement.querySelector('.popup__input_type_card-name');
+const linkInput = newCardFormElement.querySelector('.popup__input_type_url');
+
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
 
 function renderCards(cardsData) {
   cardsData.forEach(cardData => {
     const card = createCard(cardData, deleteCard, toggleLike);
     cardsContainer.append(card);
   });
+}
+
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  closePopup(editPopup);
+}
+
+function handleNewCardSubmit(evt) {
+  evt.preventDefault();
+
+  const name = titleInput.value;
+  const link = linkInput.value;
+
+  const card = createCard({ name, link }, deleteCard, toggleLike);
+
+  closePopup(newCardPopup);
+  
+  titleInput.value = '';
+  linkInput.value = '';
+
+  cardsContainer.prepend(card);
+}
+
+function fillEditForm() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
 }
 
 renderCards(initialCards);
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,51 +1,5 @@
-import { createCard, deleteCard, toggleLike } from './card';
-
-const cardsContainer = document.querySelector('.places__list');
-
 const allPopups = document.querySelectorAll('.popup');
-const editPopup = document.querySelector('.popup_type_edit');
 const imagePopup = document.querySelector('.popup_type_image');
-const newCardPopup = document.querySelector('.popup_type_new-card');
-
-const editFormElement = editPopup.querySelector('.popup__form');
-const nameInput = editFormElement.querySelector('.popup__input_type_name');
-const jobInput = editFormElement.querySelector('.popup__input_type_description');
-
-const newCardFormElement = newCardPopup.querySelector('.popup__form');
-const titleInput = newCardFormElement.querySelector('.popup__input_type_card-name');
-const linkInput = newCardFormElement.querySelector('.popup__input_type_url');
-
-const profileName = document.querySelector('.profile__title');
-const profileDescription = document.querySelector('.profile__description');
-
-function handleEditFormSubmit(evt) {
-  evt.preventDefault();
-
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
-
-  closePopup(editPopup);
-}
-
-function handleNewCardSubmit(evt) {
-  evt.preventDefault();
-
-  const name = titleInput.value;
-  const link = linkInput.value;
-
-  const card = createCard({ name, link }, deleteCard, toggleLike);
-  cardsContainer.prepend(card);
-
-  titleInput.value = '';
-  linkInput.value = '';
-
-  closePopup(newCardPopup);
-}
-
-function fillEditForm() {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileDescription.textContent;
-}
 
 function escapeHandler(evt) {
   if (evt.key === 'Escape') {
@@ -79,7 +33,4 @@ export {
   closePopup, 
   imagePopup, 
   fillImagePopup, 
-  fillEditForm, 
-  handleEditFormSubmit, 
-  handleNewCardSubmit, 
 };
