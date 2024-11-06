3a78702922a078339afdc6ef7b0aa577e38df56c
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2024-11-06 14:34:39 +0100

    Добавила обработчики добавления лайка и удаления карточек

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,30 +1,87 @@
+import { openPopup, closePopup } from "./modal";
+import { config, deleteCardRequest, putLikeRequest, deleteLikeRequest } from "./api";
+
 const cardTemplate = document.querySelector('#card-template').content;
 
-function createCard(cardData, onDelete, onLike, onImageClick) {
+function createCard(cardData, currentUserID, onDelete, onLike, onImageClick) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
   const imageElement = cardElement.querySelector('.card__image');
   const titleElement = cardElement.querySelector('.card__title');
   const deleteButtonElement = cardElement.querySelector('.card__delete-button');
   const likeButtonElement = cardElement.querySelector('.card__like-button');
+  const likeCounterElement = cardElement.querySelector('.card__like-counter');
+
+  const confirmDeletePopup = document.querySelector('.popup_type_confirm-delete');
+  const confirmDeleteButton = confirmDeletePopup.querySelector('.popup__button');
+
+  const likes = cardData.likes;
 
   imageElement.src = cardData.link;
   imageElement.alt = cardData.name;
   titleElement.textContent = cardData.name;
+  likeCounterElement.textContent = cardData.likes.length;
 
-  deleteButtonElement.addEventListener('click', onDelete);
-  likeButtonElement.addEventListener('click', onLike);
+  if (cardData.owner._id !== currentUserID) {
+    deleteButtonElement.classList.add('card__delete-button_inactive');
+  }
+
+  likes.forEach(like => {
+    const userLike = like._id;
+
+    if (userLike === currentUserID) {
+      likeButtonElement.classList.add('card__like-button_is-active');
+    }
+  });
+
+  deleteButtonElement.addEventListener('click', () => {
+    openPopup(confirmDeletePopup);
+
+    confirmDeleteButton.onclick = () => {
+      onDelete(cardElement, cardData._id);
+      closePopup(confirmDeletePopup);
+    };
+  });
+
+  likeButtonElement.addEventListener('click', () => {
+    onLike(likeButtonElement, likeCounterElement, cardData._id);
+  });
+  
   imageElement.addEventListener('click', onImageClick);
 
   return cardElement;
 };
 
-function deleteCard(evt) {
-  evt.target.parentElement.remove();
+function deleteCard(cardElement, cardId) {
+  deleteCardRequest(config, cardId)
+    .then(() => {
+      cardElement.remove();
+    })
+    .catch((error) => {
+      console.error(error);
+    })
 }
 
-function toggleLike(evt) {
-  evt.target.classList.toggle('card__like-button_is-active');
+function toggleLike(likeButton, likeCounter, cardId) {
+  if (likeButton.classList.contains('card__like-button_is-active')) {
+    deleteLikeRequest(config, cardId)
+      .then((cardData) => {
+        likeCounter.textContent = cardData.likes.length;
+        likeButton.classList.remove('card__like-button_is-active');
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  } else {
+    putLikeRequest(config, cardId)
+      .then((cardData) => {
+        likeCounter.textContent = cardData.likes.length;
+        likeButton.classList.add('card__like-button_is-active');
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
 }
 
 export { createCard, deleteCard, toggleLike };
